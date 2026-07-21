91a092c051a417440adddc96596a6e50bbb36ec2
Marcin Pietrzak <marcin@iworks.pl>
2026-07-21 09:47:46 +0200

    update build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,13 +83,13 @@ module.exports = function(grunt) {
 
 		replace_patterns: [{
 			match: /AUTHOR_NAME/g,
-			replace: '<%= pkg.author[0].name %>'
+			replace: '<%= pkg.contributors[0].name %>'
 		}, {
 			match: /AUTHOR_URI/g,
-			replace: '<%= pkg.author[0].uri %>'
+			replace: '<%= pkg.contributors[0].uri %>'
 		}, {
 			match: /AUTHOR_URL/g,
-			replace: '<%= pkg.author[0].uri %>'
+			replace: '<%= pkg.contributors[0].uri %>'
 		}, {
 			match: /BUILDTIMESTAMP/g,
 			replace: buildtimestamp
@@ -134,6 +134,9 @@ module.exports = function(grunt) {
 			replace: '<%= pkg.title %>'
 		}, {
 			match: /PLUGIN_URI/g,
+			replace: '<%= pkg.homepage %>'
+		}, {
+			match: /PLUGIN_URL/g,
 			replace: '<%= pkg.homepage %>'
 		}, {
 			match: /PLUGIN_VERSION/g,
